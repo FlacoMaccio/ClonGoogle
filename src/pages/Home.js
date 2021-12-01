9c1a590196dd507f8ec61015a0b9893900596c03
FlacoMaccio <frnamaccio890@gmail.com>
2021-11-30 22:22:48 -0300

    primer commit clongoogle

---
+++ src/pages/Home.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import AppsIcon from '@material-ui/icons/Apps'
+import "./home.css"
+import { Avatar, IconButton } from '@material-ui/core'
+import { Link } from 'react-router-dom'
+
+
+const Home = () => {
+    return (
+        <div className="home">
+            <div className="home_header">
+                <div className="home_headerLeft">
+                    <Link to="/about">Sobre Google</Link>
+                    <Link to="/store">Tienda</Link>
+                </div>
+                <div className="home_headerRight">
+                    <Link to="/gmail">Gmail</Link>
+                    <Link to="/images">Imagenes</Link>
+                    <IconButton>
+                    <AppsIcon/>
+                    </IconButton>
+                    <Avatar/>
+                </div>
+            </div>
+            <div className="home_body">                
+                <img src="https://1000marcas.net/wp-content/uploads/2020/02/logo-Google.png"/>
+                <h1>Aca va la busqueda de Google</h1>
+            </div>            
+        </div>
+    )
+}
+
+export default Home
+
